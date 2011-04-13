25ca1ec5d141c948084e6e98455ce4746c977446
Hanna Seweryn <hanka.seweryn@llp.pl>
2011-04-13 14:41:28 +0200

    view

--- models/manager.rb
+++ models/manager.rb
@@ -20,7 +20,7 @@ class Manager
   def generate
     template = File.read(File.expand_path('views/report.haml'))
     haml_engine = Haml::Engine.new(template)
-    output = haml_engine.render(@data)
+    haml_engine.render(@data)
   end
 
   def set_date(date = nil)
