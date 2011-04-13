06f1815b54d98d280bd20d0782d6ae5550aeb255
bartes <bartosz.knapik@llp.pl>
2011-04-13 12:29:53 +0200

    added report haml

--- models/manager.rb
+++ models/manager.rb
@@ -3,4 +3,13 @@ class Manager
   def parse
 
   end
+
+
+  def generate
+    template = File.read(File.expand_path('../views/report.haml'))
+    haml_engine = Haml::Engine.new(template)
+    output = haml_engine.render
+  end
+
+
 end
