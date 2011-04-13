c596226a37f295864fe5d89ac97542f7bf912558
bartes <bartosz.knapik@llp.pl>
2011-04-13 12:51:59 +0200

    added styles

--- models/manager.rb
+++ models/manager.rb
@@ -8,7 +8,7 @@ class Manager
   def generate
     template = File.read(File.expand_path('../views/report.haml'))
     haml_engine = Haml::Engine.new(template)
-    output = haml_engine.render
+    output = haml_engine.render(:total_lines => 0, :per_hour => 0)
   end
 
 
